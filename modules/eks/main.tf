1f4cb1eec0ea044e6f3446c06dbf0227a7b2a69c
devinmoreland <devinmoreland20@gmail.com>
2022-08-30 17:09:09 -0400

    sg

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -119,18 +119,14 @@ resource "aws_security_group" "node_group_one" {
     to_port   = 22
     protocol  = "tcp"
 
-    cidr_blocks = [
-      "0.0.0.0/0",
-    ]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     from_port = 80
     to_port   = 80
     protocol  = "tcp"
 
-    cidr_blocks = [
-      "0.0.0.0/0",
-    ]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     from_port = 30010
@@ -140,5 +136,11 @@ resource "aws_security_group" "node_group_one" {
     cidr_blocks = [
       "0.0.0.0/0",
     ]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
