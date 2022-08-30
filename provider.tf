2e9024a06d28e777cdd73f2b7b13c458f6cc815b
devinmoreland <devinmoreland20@gmail.com>
2022-08-30 12:41:35 -0400

    test

--- provider.tf
+++ provider.tf
@@ -20,15 +20,16 @@ terraform {
 }
 
 
-
-data "aws_eks_cluster_auth" "example" {
-  name = "example"
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
 }
-
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
 provider "kubernetes" {
   cluster_ca_certificate = base64decode(module.eks.kubeconfig-certificate-authority-data)
-  host                   = module.eks.cluster_endpoint
-  token                  = data.aws_eks_cluster_auth.example.token
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  token                  = data.aws_eks_cluster_auth.cluster.token
 
 }
 
